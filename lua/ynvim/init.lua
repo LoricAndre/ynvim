032a6a8d540b97edcd84530b16a68a9244e0203b
Loric ANDRE <loric.a@padoa-group.com>
2022-05-02 10:43:34 +0200

    Revert "fix str concat"
    This reverts commit 05635639752130b150c835a402385a6a9c018bdb.

--- lua/ynvim/init.lua
+++ lua/ynvim/init.lua
@@ -3,10 +3,10 @@ local utils = require'ynvim.utils'
 local M = {}
 
 M.getConfig = function(filename)
-  local baseDir = vim.fn.stdpath('config') .. '/'
-  local config = utils.load(baseDir .. filename)
+  local baseDir = vim.fn.stdpath('config') + '/'
+  local config = utils.load(baseDir + filename)
   for _, filename in ipairs(config.includes or {}) do
-    local file = utils.load(baseDir .. filename)
+    local file = utils.load(baseDir + filename)
     config = utils.merge(config, file)
   end
   return config
